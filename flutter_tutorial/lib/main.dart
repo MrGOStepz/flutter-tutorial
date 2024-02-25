512a1e2761ce2300cc81141eb8f1551da77e4c5b
Warawat Suksakulwat <MrGOStepz@gmail.com>
2024-02-25 22:20:40 +0700

    Initial Project

---
+++ flutter_tutorial/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const FlutterHome(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class FlutterHome extends StatefulWidget {
+  const FlutterHome({super.key, required this.title});
+  final String title;
+
+  @override
+  State<FlutterHome> createState() => _FlutterHomeState();
+}
+
+class _FlutterHomeState extends State<FlutterHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 45, 7, 98),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )
+        ),
+        child: const Center(
+          child: Text('Hello World',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),),
+        ),
+      ),
+    );
+  }
+
+}
