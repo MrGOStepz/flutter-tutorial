9c06b385dcc5fe8af4f25066ed0dd4f9edc7fcd5
Warawat Suksakulwat <mrgostepz@gmail.com>
2024-03-06 00:02:59 +0700

    Add New Tutorial Meals

---
+++ flutter_tutorial/lib/meals/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/meals/screens/categories.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+    brightness: Brightness.dark,
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      title: 'Material App',
+      home: CategoriesScreen(),
+    );
+  }
+}
